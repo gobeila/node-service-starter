6f8c75ca74175c0743ca9b79d522a624f9ef2133
Alexandre Gobeil <alexandre.gobeil@desjardins.com>
2019-05-01 09:43:56 -0400

    Ajout d'un test credible

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
-require("@babel/polyfill");
-const Koa = require("koa");
-const Router = require("koa-router");
+import "@babel/polyfill";
+import Koa from "koa";
+import Router from "koa-router";
+
 const router = new Router();
 const app = new Koa();
 const PORT = process.env.PORT || 8081;
+
 router.get("/", async ctx => {
-  ctx.body = {
-    data: "Sending some JSON"
-  };
+    ctx.body = {
+        data: "Sending some JSON",
+        person: {
+            name: "Valentino",
+            lastname: "Gagliardi",
+            role: "Web Developer",
+            age: 32
+        }
+    };
 });
+
 app.use(router.routes());
+
 const server = app.listen(PORT).on("error", err => {
-  console.error(err);
+    console.error(err);
 });
-module.exports = server;
+
+export default server;
--- test/index.routes.test.js
+++ test/index.routes.test.js
@@ -1,16 +1,18 @@
-// require the Koa server
-const server = require("../src/app");
-// require supertest
-const request = require("supertest");
-// close the server after each test
+import server from "../src/app";
+import request from "supertest";
+
 afterEach(() => {
     server.close();
 });
+
 describe("routes: index", () => {
     test("should respond as expected", async () => {
         const response = await request(server).get("/");
         expect(response.status).toEqual(200);
         expect(response.type).toEqual("application/json");
         expect(response.body.data).toEqual("Sending some JSON");
+        expect(Object.keys(response.body.person)).toEqual(
+            expect.arrayContaining(["name", "lastname", "role", "age"])
+        );
     });
 });
