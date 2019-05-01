19b6e393b2ffe37d3ed8838a857f273136b655b8
Alexandre Gobeil <alexandre.gobeil@desjardins.com>
2019-05-01 09:06:43 -0400

    jest+koa integration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
-import {add, multiply} from './math';
-
-const num1 = 5, num2 = 10;
-
-console.log('Add: ', add(num1, num2));
-console.log('Multiply: ', multiply(num1, num2));
+require("@babel/polyfill");
+const Koa = require("koa");
+const Router = require("koa-router");
+const router = new Router();
+const app = new Koa();
+const PORT = process.env.PORT || 8081;
+router.get("/", async ctx => {
+  ctx.body = {
+    data: "Sending some JSON"
+  };
+});
+app.use(router.routes());
+const server = app.listen(PORT).on("error", err => {
+  console.error(err);
+});
+module.exports = server;
